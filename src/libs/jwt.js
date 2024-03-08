697996dc9f39dc43629676bb55037ac5470cdd5f
Davidgn123 <judagu03@gmail.com>
2024-03-08 15:06:37 -0500

    Sin autentificaciones

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,23 +1,27 @@
-import { TOKEN_SECRET } from "../config.js";
-import jwt from 'jsonwebtoken'
-
+//import { TOKEN_SECRET } from "../config.js";
+//import jwt from 'jsonwebtoken'
 
 export function createAccessToken(payload) {
-   return new Promise((resolve, reject)=>{
-
-    jwt.sign(
-        payload,
-        TOKEN_SECRET,
-       {
-        expiresIn : "1d",
-       },
-        (err, token) => {
-           if (err) reject(err)
-           resolve(token)  
-        }
-       );
-
-   })
-
+   return "dummy_access_token";
 }
+
+
+//export function createAccessToken(payload) {
+//   return new Promise((resolve, reject)=>{
+
+//    jwt.sign(
+//        payload,
+//        TOKEN_SECRET,
+//       {
+//        expiresIn : "1d",
+//       },
+//       (err, token) => {
+//           if (err) reject(err)
+//           resolve(token)  
+//        }
+//       );
+
+//   })
+
+//}
 
--- src/routes/menor.routes.js
+++ src/routes/menor.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { authRequired } from "../middlewares/validateToken.js";
+//import { authRequired } from "../middlewares/validateToken.js";
 import { getMenor, getMenores,createMenor,deleteMenor, updateMenor } from "../controllers/menor.controller.js";
 
 const router = Router()
 
-router.get('/menor', authRequired, getMenores );
-router.get('/menor/:id', authRequired, getMenor);
-router.post('/menor', authRequired, createMenor );
-router.delete('/menor/:id', authRequired, deleteMenor );
-router.put('/menor/:id', authRequired, updateMenor);
+router.get('/menor',  getMenores );
+router.get('/menor/:id',  getMenor);
+router.post('/menor',  createMenor );
+router.delete('/menor/:id',  deleteMenor );
+router.put('/menor/:id',  updateMenor);
 
 export default router
