2f314dc1f6d4f59e8f9d1da91715b6228bca35fc
Rubén Velasco <129116167+Velasco182@users.noreply.github.com>
2024-03-24 19:31:19 -0500

    Registro funcionando

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import validateSchema from "../middlewares/verificarToken.js";
+//import validateSchema from "../middlewares/verificarToken.js";
 import {login, register, logout, profile, usuarios }  from "../controllers/auth.controller.js"
 import {authRequired} from '../middlewares/validateToken.js'
 
 const router = Router()
 
-router.post('/register', validateSchema , register);
+router.post('/register', register);//, validateSchema 
 
 router.post('/login',   login);
 
